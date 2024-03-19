25113cef7e9120183476902c4f723cc352878e96
Luke <lbmoogs@gmail.com>
2024-03-19 23:12:39 +0000

    add: Add calendar functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BpkCode } from '@skyscanner/backpack-web/bpk-component-code';
 import BpkButton from '@skyscanner/backpack-web/bpk-component-button';
 import BpkText from '@skyscanner/backpack-web/bpk-component-text';
+import Calendar from './components/Calendar.js';
 
 import { cssModules } from '@skyscanner/backpack-web/bpk-react-utils';
 
@@ -13,14 +14,15 @@ const App = () => (
   <div className={getClassName('App')}>
     <header className={getClassName('App__header')}>
       <div className={getClassName('App__header-inner')}>
-        <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}>Welcome to React + Backpack</BpkText>
+        <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}>Flight Schedule</BpkText>
       </div>
     </header>
     <main className={getClassName('App__main')}>
       <BpkText tagName="p" className={getClassName('App__text')}>
         To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to reload.
       </BpkText>
-      <BpkButton onClick={() => alert('It works!')}>Click me</BpkButton>
+      <Calendar/>
+      <BpkButton onClick={() => alert('It works!')}>Continue</BpkButton>
     </main>
   </div>
 );
